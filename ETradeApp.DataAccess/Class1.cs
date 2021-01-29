2dd085d26d8be091a23007bef3ac6c3048c4defb
Davutb5454 <67680075+Davutb5454@users.noreply.github.com>
2021-01-29 11:03:56 +0300

    Proje dosyası ekle.

---
+++ ETradeApp.DataAccess/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETradeApp.DataAccess
+{
+    public class Class1
+    {
+    }
+}
